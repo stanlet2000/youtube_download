2b7f6d83ff2379cdbf409e238fc310103131a932
stanlet2000 <stanlet2000@gmail.com>
2024-08-13 14:13:39 +0800

    update downloader and gui

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import os
 import tkinter as tk
 from tkinter import messagebox, filedialog
-from downloader import download_youtube_video
+from downloader import YouTubeDownloader
 import multiprocessing
 
 def select_download_path(download_path):
@@ -9,25 +9,25 @@ def select_download_path(download_path):
     if path:
         download_path.set(path)
 
-def download_button_click(url_entry, download_path):
+def download_button_click(url_entry, download_path, func):
     url = url_entry.get()
     output_path = download_path.get()
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     try:
-        result = download_youtube_video(url, output_path, convert_to='mp3')
+        result = func(url, output_path, convert_to='mp3')
         messagebox.showinfo("Success", result)
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
-def main():
+def main_window(download_func, download_path=None):
     # 创建 GUI 应用程序的主窗口
     root = tk.Tk()
     root.title("YouTube Downloader")
 
     # 创建一个存储下载路径的变量
     download_path = tk.StringVar()
-    download_path.set(os.path.expanduser("~/Downloads"))  # 默认下载路径为用户的主目录
+    download_path.set(os.path.join(os.path.expanduser("~"),"Downloads"))  # 默认下载路径为用户的下載
 
     # 创建并放置标签、输入框和按钮
     tk.Label(root, text="Enter YouTube URL:").pack(pady=10)
@@ -38,7 +38,7 @@ def main():
     tk.Entry(root, textvariable=download_path, width=50).pack(pady=5)
     tk.Button(root, text="Browse", command=lambda: select_download_path(download_path)).pack(pady=5)
 
-    tk.Button(root, text="Download", command=lambda: download_button_click(url_entry, download_path)).pack(pady=20)
+    tk.Button(root, text="Download", command=lambda: download_button_click(url_entry, download_path, download_func)).pack(pady=20)
 
     # 运行 GUI 主循环
     root.mainloop()
@@ -46,4 +46,5 @@ def main():
 if __name__ == "__main__":
     print("!!!")
     multiprocessing.freeze_support()
-    main()
+    ytd = YouTubeDownloader()
+    main_window()
